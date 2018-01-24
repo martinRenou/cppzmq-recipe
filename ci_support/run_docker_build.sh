889413fcb87a683812d897c03aef74926efcb863
Johan Mabille <johan.mabille@gmail.com>
2018-01-24 10:26:41 +0100

    CI uploads on QuantStack

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -13,7 +13,7 @@ docker info
 config=$(cat <<CONDARC
 
 channels:
- - gouarin
+ - QuantStack
  - conda-forge
  - defaults
 
@@ -60,7 +60,7 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 conda build /recipe_root --quiet || exit 1
-upload_or_check_non_existence /recipe_root gouarin --channel=main || exit 1
+upload_or_check_non_existence /recipe_root QuantStack --channel=main || exit 1
 
 touch /feedstock_root/build_artefacts/conda-forge-build-done
 EOF
